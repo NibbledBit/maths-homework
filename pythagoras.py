9f6d836175c7f52e87c799c155e9aa1f877b1116
Nibbles <me@nibbledbit.co.uk>
2022-02-24 21:26:26 +0000

    prints for help

--- pythagoras.py
+++ pythagoras.py
@@ -2,11 +2,22 @@ import math
 
 
 def pythagoras(a, b):
+    print(f"A = {a}")
+    print(f"B = {b}")
+
     a2 = a ** 2
+    print(f"A² = {a2}")
+
     b2 = b ** 2
+    print(f"B² = {b2}")
+
     c2 = a2 + b2
+    print(f"C² = {c2}")
+
     c = math.sqrt(c2)
+
+    print("A² + B² = C²")
     return c
 
 
-print(pythagoras(3, 4))
+print(f"Hypotenuse = {pythagoras(3, 4)}")
